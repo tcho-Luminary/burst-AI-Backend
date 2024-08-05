0e0b2696dabed9acae426724006cd0aa89585c5c
tcho-Luminary <tcholuminary@gmail.com>
2024-08-05 14:44:23 +0500

    Update knowledge_base.py

--- knowledge_base.py
+++ knowledge_base.py
@@ -1,82 +1,11 @@
-    if cleaned_question == "hello":
-        print("Bot: Hello! How can I Help You.")
-    elif cleaned_question == "hey":
-        print("Bot: hey")
-    elif cleaned_question == "Tell me internet password":
-        print("i am not allowed to do such tasks.")
-    elif cleaned_question == "(what is the capital of france)":
-        print("The capital of France is Paris.")
-    elif cleaned_question == "(what is the largest planet in our solar system)":
-        print("The largest planet in our solar system is Jupiter.")
-    elif cleaned_question == "(who is the CEO of apple)":
-        print("The CEO of Apple is Tim Cook.")
-    elif cleaned_question == "(what is the highest mountain in the world)":
-        print("The highest mountain in the world is Mount Everest.")
-    elif cleaned_question == "(what is the biggest mammal on earth)":
-        print("The biggest mammal on earth is the blue whale.")
-    elif cleaned_question == "(what is the smallest country in the world)":
-        print("The smallest country in the world is Vatican City.")
-    elif cleaned_question == "(who wrote the book to kill a mockingbird)":
-        print("Harper Lee wrote the book To Kill a Mockingbird.")
-    elif cleaned_question == "(what is the chemical symbol for gold)":
-        print("The chemical symbol for gold is Au.")
-    elif cleaned_question == "(how many oceans are there in total)":
-        print("There are 5 oceans in total: Pacific, Atlantic, Indian, Arctic, and Southern.")
-    elif cleaned_question == "(what is the highest mountain peak in north america)":
-        print("The highest mountain peak in North America is Denali (formerly known as Mount McKinley).")
-    elif cleaned_question == "(who was the first man to walk on the moon)":
-        print("Neil Armstrong was the first man to walk on the moon.")
-    elif cleaned_question == "(what is the largest living species of lizard)":
-        print("The largest living species of lizard is the Komodo dragon.")
-    elif cleaned_question == "(what is the largest city in china)":
-        print("The largest city in China is Shanghai.")
-    elif cleaned_question == "(who was the founder of google)":
-        print("Larry Page and Sergey Brin were the founders of Google.")
-    elif cleaned_question == "tell me a social media app":
-        print("Bot: Here are is a List of Social Media Apps: ")
-        print("Facebook link = https://facebook.com")
-        print("Twitter link = https://twitter.com")
-        print("Instagram link = https://instagram.com")
-        print("TikTok link = https://tiktok.com")
-        print("YouTube link = https://youtube.com")
-        print("WhatsApp link = https://whatsapp.com")
-        print("Reddit link = https://reddit.com")
-        print("LinkedIn link = https://linkedin.com")
-        print("Pinterest link = https://pinterest.com")
-        print("Snapchat link = https://snapchat.com")
-        print("Tumblr link = https://tumblr.com")
-        print("Discord link = https://discord.com")
-        print("Vimeo link = https://vimeo.com")
-        print("Flickr link = https://flickr.com")
-        print("Medium link = https://medium.com")
-        print("Periscope link = https://periscope.tv")
-        print("Pinterest link = https://pinterest.com")
-        print("SoundCloud link = https://soundcloud.com")
-        print("Spotify link = https://spotify.com")
-        print("Twitch link = https://twitch.tv")
-        print("WeChat link = https://weixin.qq.com")
-        print("Weibo link = http://weibo.com")
-        print("KakaoTalk link = http://story.kakaocorp.com/service/kakaotalk/")
-        print("Kik Messenger link = http://kik.me/")
-        print("Telegram link = https://telegram.org/")
-        print("Viber link = http://viber.com/")
-        print("Skype link = https://skype.live/")
-        print("Hangouts link = https://hangouts.google.com/")
-        print("Google+ link = http://plus.google.com/")
-        print("Foursquare link = http://foursquare.com/")
-        print("Flickr link = http://flickr.com/")
-        print("StumbleUpon link = http://stumbleupon.com/")
-        print("Dribbble link = http://dribbble.com/")
-        print("Goodreads link = http://goodreads.com/")
-        print("Flipboard link = http://flipboard.com/")
-        print("Quora link = http://quora.com/")
-        print("QuoraSpaces link = http://spaces.quora.com/")
-        print("Groupon link = http://groupon.com/")
-        print("Wane-lo link = http://wanelo.com/")
-        print("Shopify link = http://shopify.com/")
-    elif cleaned_question == "exit":
-        break
-    elif cleaned_question == "quit":
-        break
-    else:
-        print("I didn't understand that. Please try again.")
+def fetch_wikipedia_url(question):
+    search_url = f"https://en.wikipedia.org/w/index.php?search={question.replace(' ', '+')}"
+    response = requests.get(search_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    search_results = soup.select('.mw-search-results')
+    if search_results:
+        first_result = search_results[0].select_one('a')
+        if first_result:
+            article_url = f"https://en.wikipedia.org{first_result.get('href')}"
+            return article_url
+    return None
